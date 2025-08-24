0cb7f63ce77d7b9731470ac99b7476097d34bee1
Jonathan Moore <jonathanmoorephd@gmail.com>
2025-08-24 14:21:23 -0400

    Remove unintended setting

--- emacs-custom.el
+++ emacs-custom.el
@@ -12,7 +12,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ignored-local-variable-values '((toc-org-max-depth . 3)))
  '(package-selected-packages
    '(adaptive-wrap anzu auctex-latexmk auto-highlight-symbol browse-kill-ring cape cdlatex
                    color-moccur color-theme-modern company company-auctex
