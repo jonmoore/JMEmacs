3ce51f3c585443613e09d54d6a8aacf91d406694
Jonathan Moore <jonathanmoorephd@gmail.com>
2025-08-24 12:51:34 -0400

    Automatic update of packages
    Formatting is changed, maybe automatically.

--- emacs-custom.el
+++ emacs-custom.el
@@ -14,7 +14,26 @@
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values '((toc-org-max-depth . 3)))
  '(package-selected-packages
-   '(org-hide-drawers consult-flycheck el-patch consult-lsp embark-consult gitlab-lsp lsp-inline-completions org-super-agenda cape consult-flyspell org-transclusion minimap corfu dired-preview simple-call-tree vertico orderless marginalia embark consult cov py-vterm-interaction adaptive-wrap anzu auctex-latexmk auto-highlight-symbol browse-kill-ring cdlatex color-moccur color-theme-modern company company-auctex company-quickhelp company-restclient conda csv-mode dap-mode diminish dired-subtree disable-mouse ebib elmacro esup expand-region flycheck free-keys git-gutter-fringe gptel graphviz-dot-mode haskell-mode helm helm-ag helm-company helm-descbinds helm-lsp helm-org-rifle helm-projectile helm-rg helm-swoop htmlize hydra jq-mode js2-mode json-mode lean4-mode live-py-mode lorem-ipsum lsp-mode lsp-pyright lsp-treemacs lsp-ui macrostep magit markdown-mode maxframe mediawiki mermaid-mode multiple-cursors nexus ob-mermaid ob-restclient org org-chef org-jira org-ref ox-jira ox-mediawiki ox-reveal ox-rst projectile quelpa quelpa-use-package racket-mode rainbow-delimiters realgud restclient shut-up sicp smartparens sphinx-doc svg-lib toc-org transpose-frame treemacs treemacs-projectile undo-tree unicode-fonts visual-fill-column which-key writeroom-mode yaml-mode yasnippet yasnippet-snippets zop-to-char)))
+   '(adaptive-wrap anzu auctex-latexmk auto-highlight-symbol browse-kill-ring cape cdlatex
+                   color-moccur color-theme-modern company company-auctex
+                   company-quickhelp company-restclient conda consult consult-flycheck
+                   consult-flyspell consult-lsp corfu cov csv-mode dap-mode diminish
+                   dired-preview dired-subtree disable-mouse ebib el-patch elmacro embark
+                   embark-consult esup expand-region flycheck free-keys git-gutter-fringe
+                   gitlab-lsp gptel graphviz-dot-mode haskell-mode helm helm-ag
+                   helm-company helm-descbinds helm-lsp helm-org-rifle helm-projectile
+                   helm-rg helm-swoop htmlize hydra jq-mode js2-mode json-mode lean4-mode
+                   live-py-mode lorem-ipsum lsp-inline-completions lsp-mode lsp-pyright
+                   lsp-treemacs lsp-ui macrostep magit marginalia markdown-mode maxframe
+                   mediawiki mermaid-mode minimap multiple-cursors nexus ob-mermaid
+                   ob-restclient orderless org org-chef org-hide-drawers org-jira org-ref
+                   org-super-agenda org-transclusion ox-jira ox-mediawiki ox-reveal ox-rst
+                   projectile py-vterm-interaction quelpa quelpa-use-package racket-mode
+                   rainbow-delimiters realgud restclient show-font shut-up sicp
+                   simple-call-tree smartparens sphinx-doc svg-lib toc-org transpose-frame
+                   treemacs treemacs-projectile undo-tree unicode-fonts vertico
+                   visual-fill-column which-key writeroom-mode yaml-mode yasnippet
+                   yasnippet-snippets zop-to-char)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
